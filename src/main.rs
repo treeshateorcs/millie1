7210fa2b754195f07c69da0737907358865b5503
treeshateorcs <trees@hateorcs.com>
2022-06-29 08:48:32 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("millie1")]
+async fn main() {
+  let hero_set = Texture2D::from_file_with_format(include_bytes!("../assets/hero.png"), None);
+  //let hero_set = load_texture("assets/hero.png").await.unwrap();
+  let (mut w, mut h) = (48., 64. * 2.);
+  let (mut x, mut y) = (screen_width() / 2., screen_height() / 2.);
+  let mut shift_pressed;
+  loop {
+    let hero_source: Rect = Rect::new(w, h, 48., 64.);
+    clear_background(BLACK);
+    draw_texture_ex(
+      hero_set,
+      x,
+      y,
+      WHITE,
+      DrawTextureParams {
+        source: Some(hero_source),
+        ..Default::default()
+      },
+    );
+
+    if is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift) {
+      shift_pressed = 2.;
+      println!("{}", is_key_down(KeyCode::LeftShift));
+    } else {
+      shift_pressed = 1.;
+      println!("{}", is_key_down(KeyCode::LeftShift));
+    }
+    if is_key_down(KeyCode::W) {
+      y -= 5. * shift_pressed;
+      h = 0.;
+      if w < 48. * 2. {
+        w += 48.;
+      } else {
+        w = 0.
+      }
+      std::thread::sleep(std::time::Duration::from_millis(100));
+    } else if is_key_down(KeyCode::A) {
+      x -= 5. * shift_pressed;
+      h = 64. * 3.;
+      if w < 48. * 2. {
+        w += 48.;
+      } else {
+        w = 0.
+      }
+      std::thread::sleep(std::time::Duration::from_millis(100));
+    } else if is_key_down(KeyCode::S) {
+      y += 5. * shift_pressed;
+      h = 64. * 2.;
+      if w < 48. * 2. {
+        w += 48.;
+      } else {
+        w = 0.
+      }
+      std::thread::sleep(std::time::Duration::from_millis(100));
+    } else if is_key_down(KeyCode::D) {
+      x += 5. * shift_pressed;
+      h = 64. * 1.;
+      if w < 48. * 2. {
+        w += 48.;
+      } else {
+        w = 0.
+      }
+      std::thread::sleep(std::time::Duration::from_millis(100));
+    } else {
+      h = 64. * 2.;
+      w = 48. * 1.
+    }
+    println!("{}", get_fps());
+    next_frame().await
+  }
+}
